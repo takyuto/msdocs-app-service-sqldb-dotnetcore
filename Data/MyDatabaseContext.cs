921e7ff165b38c869b96594bd59da1b966df1442
takyuto <takyuto@gmail.com>
2025-08-04 04:31:01 +0000

    change

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -19,8 +19,8 @@ namespace DotNetCoreSqlDb.Data
         public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options, IHttpContextAccessor accessor)
             : base(options)
         {
-            // var conn = Database.GetDbConnection() as SqlConnection;
-            // conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
+            var conn = Database.GetDbConnection() as SqlConnection;
+            conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; } = default!;
