333d7f10d209524405b24afc572fb8d0d0df7cd0
takyuto <takyuto@gmail.com>
2025-08-04 01:34:46 +0000

    OBO connect

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Models;
+using Microsoft.Data.SqlClient;
 
 namespace DotNetCoreSqlDb.Data
 {
     public class MyDatabaseContext : DbContext
     {
-        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options, IHttpContextAccessor accessor)
             : base(options)
         {
+            var conn = Database.GetDbConnection() as SqlConnection;
+            conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; } = default!;
