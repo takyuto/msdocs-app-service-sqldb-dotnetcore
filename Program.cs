08405a3a28c872d476093d7502b441ee5183217b
takyuto <takyuto@gmail.com>
2025-08-04 01:48:17 +0000

    fix

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ if(builder.Environment.IsDevelopment())
 }
 else
 {
+    builder.Services.AddHttpContextAccessor();
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     builder.Services.AddStackExchangeRedisCache(options =>
